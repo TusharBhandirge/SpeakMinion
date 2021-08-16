fcfbe71421f335109fd1e5e1f21b0eb6bb64f98c
TusharBhandirge <tusharbhandirge@gmail.com>
2021-08-16 14:55:04 +0530

    Feat aded logic

---
+++ minionSpeak.js
@@ -0,0 +1,25 @@
+
+// var input = prompt("Enter your name");
+// alert("Wlcome "+input+ " to Translate to Yoda")
+var url="https://api.funtranslations.com/translate/minion.json";
+var btnTranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#id-textArea");
+var outputdiv = document.querySelector("#id-outputdiv");
+
+btnTranslate.addEventListener("click",function(){
+    clickEventHandler();
+});
+
+function clickEventHandler(){
+    //console.log("clicked");
+    //outputdiv.innerText = inputText.value;
+    fetch(url+"?text="+inputText.value)
+    .then(response=> response.json())
+    .then(json => {
+        console.log(json);
+        console.log(json.contents.translated);
+        var translatedText = json.contents.translated;
+        outputdiv.innerText = translatedText;
+    })
+
+};   
